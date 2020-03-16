4212d0573735535fbeccb9ae180e74bc131cc8ce
陈少汉 <chensh@gdhmail.com>
2020-03-16 11:12:39 +0800

    csh

---
+++ inwlms/src/main/java/com/koron/inwlms/constant/Constant.java
@@ -0,0 +1,15 @@
+package com.koron.inwlms.constant;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 常量类
+ * @author csh
+ * @Date 20200316
+ *
+ */
+@Component
+public class Constant {
+	
+}
+
