7dee80e406cb0c71b5fd6a55f38b217860921bd8
陈少汉 <chensh@gdhmail.com>
2020-03-24 15:44:36 +0800

    fixup

--- inwlms/src/main/java/com/koron/main/App.java
+++ inwlms/src/main/java/com/koron/main/App.java
@@ -9,6 +9,7 @@ import org.springframework.boot.ansi.AnsiColor;
 import org.springframework.boot.ansi.AnsiOutput;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -31,6 +32,7 @@ import java.util.List;
  **/
 @SpringBootApplication
 @ComponentScan(value = {"org.swan", "com.koron", "com.koron.main"})
+@ServletComponentScan
 public class App extends WebMvcConfigurationSupport {
     public static void main(String[] args) throws IOException {
         SpringApplication app = new SpringApplication(App.class);
