4d18dde21064141eedd02ec1e056ab9e81bd213b
陈少汉 <chensh@gdhmail.com>
2020-03-24 15:12:25 +0800

    常量类修改

--- inwlms/src/main/java/com/koron/util/Constant.java
+++ inwlms/src/main/java/com/koron/util/Constant.java
@@ -1,16 +1,16 @@
 package com.koron.util;
 
 import java.util.HashMap;
-import java.util.List;
 
 import org.koron.ebs.util.field.EnumElement;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class Constant {
 	/**
 	 * 登录用户在SESSION里的储存KEY
 	 */
-public static final String USER = "_user";
+	public static final String USER = "_user";
 	/**
 	 * 登陆微信号在SESSION里的储存KEY
 	 */
@@ -21,230 +21,148 @@ public static final String USER = "_user";
 	public static final String VALIDATECODE = "_validate_code";
 	// ////////////////////////接口错误说明/////////////
 	/**
-	 * 操作成功
+	 * 操作成功码
 	 */
 	public static final int MESSAGE_INT_SUCCESS = 0;
-	/**
-	 * 操作失败
-	 */
-	public static final int MESSAGE_INT_Failed = -1;
-	/**
-	 * 未登录
-	 */
-	public static final int MESSAGE_INT_NOLOGIN = 10000;
-	/**
-	 * 无操作权限
-	 */
-	public static final int MESSAGE_INT_NOMODULE = 10001;
-	/**
-	 * 密码错误
-	 */
-	public static final int MESSAGE_INT_PWDERROR = 10002;
-	public static final int MESSAGE_INT_NOPARAM = 10002;
-	/**
-	 * 传参为空(没有获取到参数)
-	 */
-	public static final int MESSAGE_INT_NULL = 10003;
-	/**
-	 * 添加操作失败
-	 */
-	public static final int MESSAGE_INT_ADDERROR = 10004;
-	/**
-	 * 修改操作失败
-	 */
-	public static final int MESSAGE_INT_EDITERROR = 10005;
-	/**
-	 * 删除操作失败
-	 */
-	public static final int MESSAGE_INT_DELERROR = 10006;
-	/**
-	 * 登录失败
-	 */
-	public static final int MESSAGE_INT_LOGINERROR = 10007;
-	/**
-	 * 更新失败
-	 */
-	public static final int MESSAGE_INT_UPDATERROR = 10008;
 	
 	/**
-	 * 上传失败
+	 * 操作成功提示
 	 */
-	public static final int MESSAGE_INT_UPLOADERROR = 10009;
+	public static final String MESSAGE_STRING_SUCCESS = "操作成功";
+	
 	/**
-	 * 查询失败
+	 * 操作失败码
 	 */
-	public static final int MESSAGE_INT_SELECTERROR = 10010;
+	public static final int MESSAGE_INT_ERROR = 1;
+	
 	/**
-	 * 参数校验异常
+	 * 操作失败提示
 	 */
-	public static final int MESSAGE_INT_PARAMS = 11002;
-	/**MESSAGE_VALIDATION_ERROR
-	 * 操作异常（比如不符合删除条件）
+	public static final String MESSAGE_STRING_ERROR = "操作失败";
+	
+	/**
+	 * 未登录码
 	 */
-	public static final int MESSAGE_INT_OPERATION=11003;
+	public static final int MESSAGE_INT_NOLOGIN = 10000;
+	
+	/**
+	 * 未登录提示
+	 */
+	public static final String MESSAGE_STRING_NOLOGIN = "未登录";
+	
+	/**
+	 * 无操作权限码
+	 */
+	public static final int MESSAGE_INT_NOMODULE = 10001;
+	
+	/**
+	 * 无操作权限提示
+	 */
+	public static final String MESSAGE_STRING_NOMODULE = "无操作权限";
+	
+	/**
+	 * 密码错误码
+	 */
+	public static final int MESSAGE_INT_PWDERROR = 10002;
+	
+	/**
+	 * 密码错误提示
+	 */
+	public static final String MESSAGE_STRING_PWDERROR = "密码错误";
+	
 	/**
 	 * 分配100个错误码给文件转换
 	 */
 	public static final int MESSAGE_INT_FILE_TRANSFER_ERROR = 20001;
+	
 	/**
-	 * 分配100个错误吗给数据库
+	 * 传参为空码
 	 */
-	public static final int MESSAGE_DBFAIL = 20101;
+	public static final int MESSAGE_INT_NULL = 20101;
+	
+	/**
+	 * 参数为空提示
+	 */
+	public static final String MESSAGE_STRING_NULL = "参数为空";
+	
+	/**
+	 * 添加失败码
+	 */
+	public static final int MESSAGE_INT_ADDERROR = 20102;
+	
+	/**
+	 * 添加失败提示
+	 */
+	public static final String MESSAGE_STRING_ADDERROR = "添加失败";
+	
+	/**
+	 * 修改失败码
+	 */
+	public static final int MESSAGE_INT_EDITERROR = 20103;
+	
+	/**
+	 * 修改失败提示
+	 */
+	public static final String MESSAGE_STRING_EDITERROR = "修改失败";
+	
+	/**
+	 * 删除失败码
+	 */
+	public static final int MESSAGE_INT_DELERROR = 20104;
+	
+	/**
+	 * 修改操作失败提示
+	 */
+	public static final String MESSAGE_STRING_DELERROR = "删除失败";
+	
+	/**
+	 * 登录失败码
+	 */
+	public static final int MESSAGE_INT_LOGINERROR = 20105;
+	
+	/**
+	 * 登录失败提示
+	 */
+	public static final String MESSAGE_STRING_LOGINERROR = "登录失败";
+	
+	/**
+	 * 上传失败码
+	 */
+	public static final int MESSAGE_INT_UPLOADERROR = 20106;
+	
+	/**
+	 * 上传失败提示
+	 */
+	public static final String MESSAGE_STRING_UPLOADERROR = "上传失败";
+	
+	/**
+	 * 查询失败码
+	 */
+	public static final int MESSAGE_INT_SELECTERROR = 20107;
+	
+	/**
+	 * 查询失败提示
+	 */
+	public static final String MESSAGE_STRING_SELECTERROR = "查询失败";
+	
+	/**
+	 * 参数校验异常码
+	 */
+	public static final int MESSAGE_INT_PARAMS = 20108;
+	
+	/**
+	 * 参数校验异常提示
+	 */
+	public static final String MESSAGE_STRING_PARAMS = "参数校验异常";
+	
+	
 	/**
 	 * 自定义表单增加辅助单位功能(在前台要自动加上辅助功能的脚本)
 	 */
 	public static final int HAS_ASSIST = 1000;
 	// ////////////////////////接口错误说明/////////////
 
-	/**
-	 * 操作成功
-	 */
-	public static final String MESSAGE_STRING_SUCCESS = "message_success";
 
-	/**
-	 * 操作失败
-	 */
-	public static final String MESSAGE_STRING_ERROR = "message_error";
-
-	/**
-	 * 未登录
-	 */
-	public static final String MESSAGE_STRING_NOLOGIN = "message_no_login";
-	/**
-	 * 无操作权限
-	 */
-	public static final String MESSAGE_STRING_NOMODULE = "message_no_module";
-	/**
-	 * 传参错误
-	 */
-	public static final String MESSAGE_STRING_NOPARAM = "message_param_error";
-	/**
-	 * 传参为空(没有获取到参数)
-	 */
-	public static final String MESSAGE_STRING_NULL = "message_null_param";
-	/**
-	 * 查询失败
-	 */
-	public static final String MESSAGE_STRING_SELECTERROR = "message_select_error";
-	/**
-	 * 添加操作失败
-	 */
-	public static final String MESSAGE_STRING_ADDERROR = "message_add_error";
-	/**
-	 * 添加操作成功
-	 */
-	public static final String MESSAGE_STRING_ADDSUCCESS = "message_add_success";
-	/**
-	 * 修改操作失败
-	 */
-	public static final String MESSAGE_STRING_EDITERROR = "message_edit_error";
-	/**
-	 * 修改操作成功
-	 */
-	public static final String MESSAGE_STRING_EDITSUCCESS = "message_edit_success";
-	/**
-	 * 删除操作失败
-	 */
-	public static final String MESSAGE_STRING_DELERROR = "message_del_error";
-	/**
-	 * 删除操作成功
-	 */
-	public static final String MESSAGE_STRING_DELSUCCESS = "message_del_success";
-	/**
-	 * 登录失败
-	 */
-	public static final String MESSAGE_STRING_LOGINERROR = "message_login_error";
-	/**
-	 * 登入成功
-	 */
-	public static final String MESSAGE_STRING_LOGINSUCCESS = "message_login_success";
-	/**
-	 * 登入成功，请完善个人信息
-	 */
-	public static final String MESSAGE_STRING_LOGINFINSHINFO = "message_finish_userinfo";
-	/**
-	 * 退出失败
-	 */
-	public static final String MESSAGE_STRING_LOGINOUTERROR = "message_loginout_error";
-	/**
-	 * 上传成功
-	 */
-	public static final String MESSAGE_STRING_UPLOADSUCCESS = "message_upload_success";
-
-	/**
-	 * 上传失败
-	 */
-	public static final String MESSAGE_STRING_UPLOADERROR = "message_upload_error";
-
-	/**
-	 * 文件已存在
-	 */
-	public static final String MESSAGE_STRING_FILE_EXIST = "message_file_exist";
-	/**
-	 * 数据已存在
-	 */
-	public static final String MESSAGE_STRING_DATA_EXIST = "message_data_exist";
-	/**
-	 * supCode 不存在
-	 */
-	public static final String MESSAGE_STRING_SUPCODE_NOTEXIST = "message_supcode_notexist";
-	/**
-	 * sourceSr 不存在
-	 */
-	public static final String MESSAGE_STRING_SOURCESR_NOTEXIST = "message_sourcesr_notexist";
-
-	/**
-	 * installationId 不存在
-	 */
-	public static final String MESSAGE_STRING_INSID_NOTEXIST = "message_insid_notexist";
-	/**
-	 * SSS No.不存在
-	 */
-	public static final String MESSAGE_STRING_SSSNO_NOTEXIST = "message_sssno_notexist";
-	/**
-	 * flometer 不存在
-	 */
-	public static final String MESSAGE_STRING_FLOMETER_NOTEXIST = "message_flometer_notexist";
-	/**
-	 * group 不存在
-	 */
-	public static final String MESSAGE_STRING_GROUP_NOTEXIST = "message_group_notexist";
-	/**
-	 * DMA不存在
-	 */
-	public static final String MESSAGE_STRING_DMA_NOTEXIST = "message_dma_notexist";
-	/**
-	 * data logger ref 不存在
-	 */
-	public static final String MESSAGE_STRING_DATALOGGERREF_NOTEXIST = "message_dataloggerref_notexist";
-	/**
-	 * 同步岗位错误
-	 */
-	public static final String MESSAGE_STRING_SYNCPOSTERROR = "message_syncpost_error";
-	/**
-	 * 执行sql操作成功
-	 */
-	public static final String MESSAGE_STRING_EXECUTESUCCESS = "message_execute_success";
-	/**
-	 * 执行sql操作失败
-	 */
-	public static final String MESSAGE_STRING_EXECUTEERROR = "message_execute_error";
-	/**
-	 * AD验证失败
-	 */
-	public static final String MESSAGE_STRING_CHECKADERROR = "message_checkad_error";
-	/**
-	 * 没有职位信息
-	 */
-	public static final String MESSAGE_STRING_NOPOST = "message_nopost_error";
-	/**
-	 * 密码错误
-	 */
-	public static final String MESSAGE_STRING_PASSWORDERROR = "message_password_error";
-	/**
-	 * 新密码不一致
-	 */
-	public static final String MESSAGE_STRING_PASSWORDNOSAME = "message_password_onsame";
 	/**
 	 * 执行sql成功返回码
 	 */
@@ -293,4 +211,44 @@ public static final String USER = "_user";
 	 */
 	public final static HashMap<String, EnumElement<Object>> enumCache = new HashMap<>();
 	
+	
+	/**
+	 * 在windown服务器的文件存储路径
+	 */
+	public final static String WIN_FILE_PATH = "C:/inwlms_file";
+	
+	/**
+	 * 在linux服务器的文件存储路径
+	 */
+	public final static String LINUX_FILE_PATH = "/usr/inms_file";
+	
+	/**
+	 * 导出列表的最大条数
+	 */
+	public final static Integer DOWN_MAX_LIMIT = 1000;
+	
+	/**
+	 * 时间类型分钟
+	 */
+	public final static Integer TIME_TYPE_MIN = 0;
+	
+	/**
+	 * 时间类型小时
+	 */
+	public final static Integer TIME_TYPE_H = 1;
+	
+	/**
+	 * 时间类型天
+	 */
+	public final static Integer TIME_TYPE_D = 2;
+	
+	/**
+	 * 时间类型月
+	 */
+	public final static Integer TIME_TYPE_M = 3;
+	
+	/**
+	 * 时间类型年
+	 */
+	public final static Integer TIME_TYPE_Y = 4;
 }
