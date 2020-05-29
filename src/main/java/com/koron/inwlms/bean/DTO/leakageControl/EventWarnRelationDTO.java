a14d9311fe3b162ba1ead4e8cce9269869a6870c
刘刚 <liug@ghmail.com>
2020-05-29 16:24:48 +0800

    事项关联

---
+++ src/main/java/com/koron/inwlms/bean/DTO/leakageControl/EventWarnRelationDTO.java
@@ -0,0 +1,21 @@
+package com.koron.inwlms.bean.DTO.leakageControl;
+
+import java.util.List;
+
+import com.koron.inwlms.bean.VO.leakageControl.EventWarnRelation;
+
+public class EventWarnRelationDTO {
+	
+	private List<EventWarnRelation> eventWarnRelationList;
+
+	public List<EventWarnRelation> getEventWarnRelationList() {
+		return eventWarnRelationList; 
+	}
+
+	public void setEventWarnRelationList(List<EventWarnRelation> eventWarnRelationList) {
+		this.eventWarnRelationList = eventWarnRelationList;
+	}
+	
+	
+
+}
