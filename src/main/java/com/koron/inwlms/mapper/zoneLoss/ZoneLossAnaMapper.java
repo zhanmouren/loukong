71f724624c62b2ff34c2ac443cd44d1bdc34c7c0
陈少汉 <chensh@gdhmail.com>
2020-06-22 09:35:20 +0800

    分区漏点信息

--- src/main/java/com/koron/inwlms/mapper/zoneLoss/ZoneLossAnaMapper.java
+++ src/main/java/com/koron/inwlms/mapper/zoneLoss/ZoneLossAnaMapper.java
@@ -1,8 +1,14 @@
 package com.koron.inwlms.mapper.zoneLoss;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.koron.ebs.mybatis.SessionFactory;
 import org.springframework.stereotype.Repository;
 
+import com.koron.inwlms.bean.VO.zoneLoss.BurstLeakAnalysisVO;
 import com.koron.inwlms.bean.VO.zoneLoss.IndicatorInfo;
+import com.koron.inwlms.bean.VO.zoneLoss.LeakDetailsVO;
 
 /**
  * 分区漏损-水平衡分析mapper
@@ -18,6 +24,17 @@ public interface ZoneLossAnaMapper {
 	 * @return
 	 */
 	IndicatorInfo queryIndicLevel2Info(String code);
-
 	
+	/**
+	 * 查询分区所有漏点信息
+	 * @return
+	 */
+	List<BurstLeakAnalysisVO> queryBurstLeakAnalysis();
+
+	/**
+	 * 根据id查询漏点详细信息
+	 * @param id
+	 * @return
+	 */
+	LeakDetailsVO queryBurstLeakById(@Param("id") String id);
 }
