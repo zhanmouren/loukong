3e8c4781bb70f0d34ac12f020b671eb9d24c7c7c
刘刚 <liug@ghmail.com>
2020-06-30 19:35:31 +0800

    更新

---
+++ src/main/java/com/koron/inwlms/bean/VO/leakageControl/StrategyStatistics.java
@@ -0,0 +1,125 @@
+package com.koron.inwlms.bean.VO.leakageControl;
+
+public class StrategyStatistics {
+	/**
+	 * 压力控制
+	 */
+	private Integer p;
+	/**
+	 * 管网捡漏
+	 */
+	private Integer l;
+	/**
+	 * 水表更换
+	 */
+	private Integer f;
+	/**
+	 * 管道改造
+	 */
+	private Integer r;
+	
+	private Integer pr;
+	
+	private Integer pl;
+	
+	private Integer pf;
+	
+	private Integer rf;
+	
+	private Integer lf;
+	
+	private Integer prf;
+	
+	private Integer plf;
+
+	public Integer getP() {
+		return p;
+	}
+
+	public void setP(Integer p) {
+		this.p = p;
+	}
+
+	public Integer getL() {
+		return l;
+	}
+
+	public void setL(Integer l) {
+		this.l = l;
+	}
+
+	public Integer getF() {
+		return f;
+	}
+
+	public void setF(Integer f) {
+		this.f = f;
+	}
+
+	public Integer getR() {
+		return r;
+	}
+
+	public void setR(Integer r) {
+		this.r = r;
+	}
+
+	public Integer getPr() {
+		return pr;
+	}
+
+	public void setPr(Integer pr) {
+		this.pr = pr;
+	}
+
+	public Integer getPl() {
+		return pl;
+	}
+
+	public void setPl(Integer pl) {
+		this.pl = pl;
+	}
+
+	public Integer getPf() {
+		return pf;
+	}
+
+	public void setPf(Integer pf) {
+		this.pf = pf;
+	}
+
+	public Integer getRf() {
+		return rf;
+	}
+
+	public void setRf(Integer rf) {
+		this.rf = rf;
+	}
+
+	public Integer getLf() {
+		return lf;
+	}
+
+	public void setLf(Integer lf) {
+		this.lf = lf;
+	}
+
+	public Integer getPrf() {
+		return prf;
+	}
+
+	public void setPrf(Integer prf) {
+		this.prf = prf;
+	}
+
+	public Integer getPlf() {
+		return plf;
+	}
+
+	public void setPlf(Integer plf) {
+		this.plf = plf;
+	}
+	
+	
+
+}
