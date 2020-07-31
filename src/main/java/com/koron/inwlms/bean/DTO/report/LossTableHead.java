d1e8440f0da2a1f16d05f80e106b6f21976d4324
liug <liug@krrj.cn>
2020-07-31 18:30:54 +0800

    更新

---
+++ src/main/java/com/koron/inwlms/bean/DTO/report/LossTableHead.java
@@ -0,0 +1,47 @@
+package com.koron.inwlms.bean.DTO.report;
+
+public class LossTableHead {
+	
+	private String code;
+	
+	private String subName;
+	
+	private String parentName;
+	
+	private String company;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getSubName() {
+		return subName;
+	}
+
+	public void setSubName(String subName) {
+		this.subName = subName;
+	}
+
+	public String getParentName() {
+		return parentName;
+	}
+
+	public void setParentName(String parentName) {
+		this.parentName = parentName;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	
+	
+
+}
